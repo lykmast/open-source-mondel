6424ebf61619b568c4cebe60bc7c3b4287707865
lykmast <lykmast@gmail.com>
2021-12-20 12:12:40 +0200

    Added some source so that it can be open.

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for open-source-mondel
+
+## Unreleased changes
---
+++ src/Lib.hs
@@ -0,0 +1,6 @@
+module Lib
+    ( assertOpenSource
+    ) where
+
+assertOpenSource :: IO ()
+assertOpenSource = putStrLn "See? Open Source!"
